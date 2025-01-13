53337cb6a601b90c7a034825e52a22e03deeecc4
cross-east-woowahan <cross.east@woowahan.com>
2025-01-13 10:30:26 +0900

    fix: MockCpuUsageMonitorClient 주석 수정

--- src/main/java/com/example/kafkathrottling/common/MockCpuUsageMonitorClient.java
+++ src/main/java/com/example/kafkathrottling/common/MockCpuUsageMonitorClient.java
@@ -2,7 +2,7 @@ package com.example.kafkathrottling.common;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-// 호출시마다 CPU 사용량을 5%씩 80%까지 증가시키는 Mock Client
+// 호출시마다 CPU 사용량을 5%씩 90%까지 증가시키는 Mock Client
 public class MockCpuUsageMonitorClient implements CpuUsageMonitorClient {
     private static final int CPU_USAGE_LIMIT = 90;
 
